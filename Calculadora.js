de167903f80d1eecdcc8a1d3108bb7452e8cc212
Diego Richard <10diieggos@gmail.com>
2020-08-21 17:28:51 -0300

    Aula 169 - Metodos estaticos

---
+++ Calculadora.js
@@ -0,0 +1,14 @@
+class Calculadora {
+  
+  static Somar(a, b) {
+    console.log(a + b);
+  };
+  
+  static Subitrair(a, b) {
+    console.log(a - b);
+  };
+
+};
+
+Calculadora.Somar(20, 10);
+Calculadora.Subitrair(20, 10);
