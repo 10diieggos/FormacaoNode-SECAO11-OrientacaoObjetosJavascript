05b96aca6f53d03805ac2ecda2ad87d05a81a48c
Diego Richard <10diieggos@gmail.com>
2020-08-22 03:42:07 -0300

    Aula 172 - Composiçao, a aula mais importante do modulo - parte1 (apresentando o problema)

---
+++ ManipuladorDeArquivo.js
@@ -0,0 +1,71 @@
+class fileHandler {
+
+  constructor (name) {
+    this.file = name;
+  };
+
+  readFile () {
+    console.log('Reading...');
+  };
+
+  writeFile (data) {
+    console.log('Writing data...');
+  };
+
+  createFile (name) {
+    console.log(`Creating file ${name}...`);
+  };
+  
+  deleteFile (name) {
+    console.log(`Deleting file ${name}...`);    
+  };
+
+};
+
+class fileHandler2 {
+
+  constructor (name) {
+    this.file = name;
+  };
+
+  readFile () {
+    console.log('Reading...');
+  };
+
+  writeFile (data) {
+    console.log('Writing data...');
+  };
+
+  createFile (name) {
+    console.log(`Creating file ${name}...`);
+  };
+  
+  deleteFile (name) {
+    console.log(`Deleting file ${name}...`);    
+  };
+
+};
+
+class fileHandler3 {
+
+  constructor (name) {
+    this.file = name;
+  };
+
+  readFile () {
+    console.log('Reading...');
+  };
+
+  writeFile (data) {
+    console.log('Writing data...');
+  };
+
+  createFile (name) {
+    console.log(`Creating file ${name}...`);
+  };
+  
+  deleteFile (name) {
+    console.log(`Deleting file ${name}...`);    
+  };
+
+};
