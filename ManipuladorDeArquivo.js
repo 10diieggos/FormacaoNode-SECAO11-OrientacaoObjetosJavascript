9c8f48403991ee9d0329c361f45b58902c76a1b4
Diego Richard <10diieggos@gmail.com>
2020-08-22 04:36:56 -0300

    Aula 172 - Composiçao, a aula mais importante do modulo - parte2 (apresentando os componentes)

--- ManipuladorDeArquivo.js
+++ ManipuladorDeArquivo.js
@@ -1,71 +1,47 @@
+class Reader {
+  constructor(file) {
+    this.file = file;
+  };
+  Read () {
+    console.log(`Content of ${this.file}`)
+  };
+};
+class Writer {
+  constructor(file) {
+    this.file = file;
+  };
+  Write (file, content) {
+    console.log(`Writed content in ${this.file}`);
+  };
+};
+class Creator {
+  constructor(file) {
+    this.file = file;
+  };
+  Create   () {
+    console.log(`File ${this.file} created...`);
+  };
+};
+class Destroyer {
+  constructor(file) {
+    this.file = file;
+  };
+  Delete () {
+    console.log(`File ${this.file} deleted...`);
+  };
+};
 class fileHandler {
-
-  constructor (name) {
-    this.file = name;
+  constructor (file) {
+    this.reader = new Reader(file);
+    this.writer = new Writer(file);
+    this.creator = new Creator(file);
+    this.destroyer = new Destroyer(file);
   };
-
-  readFile () {
-    console.log('Reading...');
-  };
-
-  writeFile (data) {
-    console.log('Writing data...');
-  };
-
-  createFile (name) {
-    console.log(`Creating file ${name}...`);
-  };
-  
-  deleteFile (name) {
-    console.log(`Deleting file ${name}...`);    
-  };
-
 };
 
-class fileHandler2 {
+let handler = new fileHandler('meuArquivo.txt');
 
-  constructor (name) {
-    this.file = name;
-  };
-
-  readFile () {
-    console.log('Reading...');
-  };
-
-  writeFile (data) {
-    console.log('Writing data...');
-  };
-
-  createFile (name) {
-    console.log(`Creating file ${name}...`);
-  };
-  
-  deleteFile (name) {
-    console.log(`Deleting file ${name}...`);    
-  };
-
-};
-
-class fileHandler3 {
-
-  constructor (name) {
-    this.file = name;
-  };
-
-  readFile () {
-    console.log('Reading...');
-  };
-
-  writeFile (data) {
-    console.log('Writing data...');
-  };
-
-  createFile (name) {
-    console.log(`Creating file ${name}...`);
-  };
-  
-  deleteFile (name) {
-    console.log(`Deleting file ${name}...`);    
-  };
-
-};
+handler.creator.Create();
+handler.writer.Write();
+handler.reader.Read();
+handler.destroyer.Delete();
